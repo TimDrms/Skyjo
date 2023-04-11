fd45529bdcedf44acf5140110abeb9af9e7ab7ca
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-11 14:25:49 +0200

    + add the setters and getters for the class Player.

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,32 @@
 public class Player {
+    private String player = new String();
+    private int nbGamesPlayed, nbGamesWon;
+    Player(){
+        this.player = null;
+        this.nbGamesPlayed = 0;
+        this.nbGamesWon = 0;
+    }
+    Player(String namePlayer){
+        this.player = namePlayer;
+        this.nbGamesPlayed = 0;
+        this.nbGamesWon = 0;
+    }
+    public void setPlayer(String namePlayer){
+        this.player = namePlayer;
+    }
+    public String getPlayer(){
+        return this.player;
+    }
+    public void setNbGamesPlayed(int nbGamesPlayed){
+        this.nbGamesPlayed = nbGamesPlayed;
+    }
+    public int getNbGamesPlayed(){
+        return this.nbGamesPlayed;
+    }
+    public void setNbGamesWon(int nbGamesWon){
+        this.nbGamesWon = nbGamesWon;
+    }
+    public int getNbGamesWon(){
+        return this.nbGamesWon;
+    }
 }
