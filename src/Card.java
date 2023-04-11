4808889376398b809eecc32314bc0dd371e979fa
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-11 14:13:23 +0200

    + add the setters and getters for value and isReturned

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,23 @@
 public class Card {
+    private int value = 0;
+    private boolean isReturned = false;
+    Card(){
+        this.value = 0;
+        this.isReturned = false;
+    }
+    Card(int value){
+        this.value = value;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public void setIsReturned(boolean isReturned){
+        this.isReturned = isReturned;
+    }
+    public boolean getIsReturded(){
+        return this.isReturned;
+    }
 }
