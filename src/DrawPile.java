505d07725d61cf73f136bf34ab03908c0cf4a8d9
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-12 22:39:31 +0200

    + added method "generateDrawPile". For the moment it doesn't work, because I have to find how to correctly pick a random card (because for the moment, there are not generated). + added method "pickDrawCard". This method should work, it prints a random card of the draw pile. We'll have to change the print by the thing that will interest us (for example, adding it in the deck).

--- src/DrawPile.java
+++ src/DrawPile.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DrawPile {
+    ArrayList<Card> drawPile = new ArrayList<>(150);
+    public void generateDrawPile(ArrayList drawPile, Game game){
+        int nbPlayers = game.getNbPlayers();
+        if(this.drawPile.isEmpty()){
+            // Generate (150 - 12 * nb_player) cards
+            // I'm not sure if my variable nbPlayers will work.
+            for(int i = 0; i < (150-12*nbPlayers); i++){
+                // Generate the cards (how to generate all the different cards ???)
+            }
+        }
+        else{
+            System.out.println("The draw pile has already been generated.");
+        }
+    }
+    public void pickDrawCard(ArrayList drawPile){
+        if(drawPile.isEmpty()){
+            System.out.println("Error: the draw pile is empty!");
+        }
+        else{
+            Collections.shuffle(this.drawPile);
+            System.out.println(this.drawPile.get(0));
+        }
+    }
 }
