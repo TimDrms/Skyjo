d42bcdf53f33d884abb4b08faf1e2a2e1eefd79b
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-12 22:36:56 +0200

    + only added the variable "nbPlayers" and the setter & getter for this variable

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,9 @@
 public class Game {
+    private int nbPlayers;
+    public void setNbPlayers(int nb){
+        this.nbPlayers = nb;
+    }
+    public int getNbPlayers(){
+        return this.nbPlayers;
+    }
 }
