4555911315a6fea7bb0753ae772c7db99496e94b
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-14 14:06:46 +0200

    + Makes test for the Card.java class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Card card1 = new Card(8, false);
+        Card card2 = new Card(12, true);
+
+        System.out.println(card1);
+        System.out.println(card2);
+
     }
 }
