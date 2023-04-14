3aae735f88413249caa00faff72031797e7adacb
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-14 17:41:23 +0200

    - put a comment for the initializeDeck. It doesn't work for the moment.

--- src/Deck.java
+++ src/Deck.java
@@ -10,7 +10,7 @@ public class Deck {
     public int getScoreRound(){
         return this.scoreRound;
     }
-    public ArrayList initializeDeck(ArrayList<ArrayList<Card>> deck){
+    public ArrayList initializeDeck(DrawPile drawPile){
         /* A FAIRE
         Initialiser les 12 cartes du jeu de manière aléatoire
         Il faudra modifier dans le Collections.shuffle() ce qu'il y a à l'intérieur comme variable. Actuellement c'est "deck", mais ça devra être la pile de carte dans DrawPile.
@@ -18,8 +18,8 @@ public class Deck {
         A FAIRE */
         for(int i = 0; i<4; i++){
             for(int j = 0; j<3; j++){
-                Collections.shuffle(deck);
-                //this.deck.get(i).set(j, value); // Insère la carte dans l'emplacement [i][j] du deck.
+                Collections.shuffle(drawPile.getDrawPile());
+                //this.deck.get(i).set(j,drawPile.getDrawPile().add(drawPile.pickDrawCard())); // Insère la carte dans l'emplacement [i][j] du deck.
             }
         }
         return this.deck;
