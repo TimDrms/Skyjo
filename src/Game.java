a92cbe1cb1abca01afff58796e557caa35cb6d4f
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-16 11:43:18 +0200

    + Adding setter and getter for actualRound. + Adding method "initializeGame" that start a new game. It hasn't been tested yet. + Adding method "stateOfTheGame". It could be used when a round is finished to check if we should end the game or start a new round. It hasn't been tested yet too.

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,54 @@
+import java.util.ArrayList;
+
 public class Game {
     private int nbPlayers;
+    ArrayList<Player> players = new ArrayList<>();
+    private Round actualRound;
     public void setNbPlayers(int nb){
         this.nbPlayers = nb;
     }
     public int getNbPlayers(){
         return this.nbPlayers;
+    }
+    public void setActualRound(Round actualRound) {
+        this.actualRound = actualRound;
+    }
+    public Round getActualRound(){
+        return this.actualRound;
+    }
+    public Round initializeGame(int nbPlayers){
+        /*
+        Hasn't been tested yet.
+         */
+        DrawPile drawPile = new DrawPile();
+        drawPile.generateDrawPile();
+        for(int i = 0 ; i < nbPlayers ; i++){
+            Player player = new Player();
+            player.askName();
+            players.add(player);
+            Deck deck = new Deck();
+            deck.initializeDeck(drawPile);
+        }
+        return new Round();
+    }
+    public void stateOfTheGame(){
+        /*
+        Hasn't been tested yet.
+         */
+        boolean isFinished = false;
+        String winner = new String();
+        for(Player player : players){
+            if(player.getScoreGame() >= 100){
+                System.out.println("The game is finished!");
+                isFinished = true;
+                winner = player.getPlayer();
+            }
+        }
+        if(isFinished == true){
+            System.out.println("Game ended! The winner is " + winner + "! Congratz!");
+        }
+        else{
+            new Round();
+        }
     }
 }
