4b80156d755c10ab0783ad43a45f7e21c1198136
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-16 11:46:22 +0200

    + Adding variable "scoreGame" which gives the total score of the game for the player. Also added the setter & getter for this variable.

--- src/Player.java
+++ src/Player.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 public class Player {
     private String player = new String();
-    private int nbGamesPlayed, nbGamesWon;
+    private int nbGamesPlayed, nbGamesWon, scoreGame;
     Player(){
         this.player = null;
         this.nbGamesPlayed = 0;
         this.nbGamesWon = 0;
+        this.scoreGame = 0;
     }
     Player(String namePlayer){
         this.player = namePlayer;
         this.nbGamesPlayed = 0;
         this.nbGamesWon = 0;
+        this.scoreGame = 0;
     }
     public void setPlayer(String namePlayer){
         this.player = namePlayer;
     }
     public String getPlayer(){
         return this.player;
+    }
+    public void setScoreGame(int scoreGame) {
+        this.scoreGame = scoreGame;
+    }
+    public int getScoreGame() {
+        return this.scoreGame;
     }
     public void setNbGamesPlayed(int nbGamesPlayed){
         this.nbGamesPlayed = nbGamesPlayed;
