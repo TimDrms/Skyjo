da5df28415d6fb98a79152f1999c2016e2c2e31b
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-04-21 22:09:50 +0200

    + Just adding the name of the different methods

--- src/Round.java
+++ src/Round.java
@@ -1,2 +1,17 @@
 public class Round {
+    public void stateOfTheRound(){
+
+    }
+
+    public void chooseBetweenPileDiscard(){
+
+    }
+
+    public void chooseBetweenDropReplace(){
+
+    }
+
+    public void replaceCard(){
+
+    }
 }
